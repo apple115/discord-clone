06493e7cca71999d7611136b27d765ef43b5be0a
apple115 <2567031030@qq.com>
2024-10-08 14:29:29 +0800

    feat: 添加channal api的设计

--- backend/routers/api/v1/channels.go
+++ backend/routers/api/v1/channels.go
@@ -2,7 +2,24 @@ package v1
 
 import "github.com/gin-gonic/gin"
 
-// @Router /channels/{channelID}/messages
-func GetMessagesByChannelID(c *gin.Context) {
+
+// POST /api/channels
+//@Router /api/channels
+//创建某个频道
+func CreateChannel(c *gin.Context) {
 	//TODO
 }
+
+
+func DeleteChannel(c *gin.Context) {
+	//TODO
+}
+
+// Get
+// @Router /channels/{channelID}/messages
+// 得到某个频道的所有消息
+func GetChannelMessageByID(c *gin.Context) {
+	//TODO
+}
+
+
