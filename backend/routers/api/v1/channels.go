cc389d41d7f287a2e7488078825e406f5f339801
apple115 <2567031030@qq.com>
2024-10-17 18:49:38 +0800

    feat: 添加了一个POST 给这个channels 发送图片

--- backend/routers/api/v1/channels.go
+++ backend/routers/api/v1/channels.go
@@ -1,10 +1,16 @@
 package v1
 
 import (
+	"context"
 	"discord-clone/models"
 	"discord-clone/pkg/app"
 	"discord-clone/pkg/e"
+	"discord-clone/pkg/upload"
+	"discord-clone/service"
+	"encoding/json"
+	"log"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -118,7 +124,7 @@ func UpdateChannel(c *gin.Context) {
 	var (
 		form  CreateChannelForm
 		appG  = app.Gin{C: c}
-		err   Error
+		err   error
 		exist bool
 	)
 	channelID := c.Param("channelID")
@@ -171,7 +177,7 @@ func DeleteChannelByID(c *gin.Context) {
 	appG := app.Gin{C: c}
 
 	//是否存在这个频道
-	exist, err = models.ExitChannel(channelID)
+	exist, err := models.ExitChannel(channelID)
 	if err != nil {
 		appG.Response(http.StatusInternalServerError, e.ERROR_CHECK_EXIST_CHANNEL_FAIL, nil)
 		return
@@ -182,10 +188,83 @@ func DeleteChannelByID(c *gin.Context) {
 	}
 
 	// 删除频道
-	err := models.DeleteChannel(channelID)
+	err = models.DeleteChannel(channelID)
 	if err != nil {
 		appG.Response(http.StatusInternalServerError, e.ERROR_DELETE_CHANNEL_FAIL, nil)
 		return
+	}
+	appG.Response(http.StatusOK, e.SUCCESS, nil)
+}
+
+func CreateChannelImage(c *gin.Context) {
+	channelID := c.Param("channeID")
+	appG := app.Gin{C: c}
+	UserId, exist := c.Get("user_id")
+	if !exist {
+		appG.Response(http.StatusBadRequest, e.ERROR_CHECK_EXIST_USER, nil)
+		return
+	}
+
+	//是否存在这个频道
+	exist, err := models.ExitChannel(channelID)
+	if err != nil {
+		appG.Response(http.StatusInternalServerError, e.ERROR_CHECK_EXIST_CHANNEL_FAIL, nil)
+		return
+	}
+	if !exist {
+		appG.Response(http.StatusOK, e.ERROR_NOT_EXIST_CHANNEL, nil)
+		return
+	}
+
+	//存储图片生成的URL
+	file, image, err := c.Request.FormFile("image")
+	if err != nil {
+		appG.Response(http.StatusInternalServerError, e.ERROR, nil)
+		return
+	}
+
+	if image == nil {
+		appG.Response(http.StatusBadRequest, e.INVALID_PARAMS, nil)
+		return
+	}
+
+	imageName := upload.GetImageName(image.Filename)
+	fullPath := upload.GetImageFullPath()
+	savePath := upload.GetImagePath()
+	src := fullPath + imageName
+
+	if !upload.CheckImageExt(imageName) || !upload.CheckImageSize(file) {
+		appG.Response(http.StatusBadRequest, e.ERROR_UPLOAD_CHECK_IMAGE_FORMAT, nil)
+		return
+	}
+	err = upload.CheckImage(fullPath)
+	if err != nil {
+		appG.Response(http.StatusInternalServerError, e.ERROR_UPLOAD_CHECK_IMAGE_FAIL, nil)
+		return
+	}
+	if err := c.SaveUploadedFile(image, src); err != nil {
+		appG.Response(http.StatusInternalServerError, e.ERROR_UPLOAD_SAVE_IMAGE_FAIL, nil)
+		return
+	}
+	content := savePath + imageName
+	message := map[string]interface{}{
+		"type": "new_message",
+		"data": map[string]interface{}{
+			"channel_id": channelID,
+			"user_id":    UserId.(uint),
+			"message_id": "abc123", // 示例消息ID
+			"content":    content,
+			"timestamp":  time.Now().Format(time.RFC3339),
+		},
+	}
+
+	messageByte, err := json.Marshal(message)
+	service.BroadcastMessageToChannel(channelID, messageByte)
+
+	collection := models.MongoDB.Collection("messages")
+	_, err = collection.InsertOne(context.TODO(), message)
+	if err != nil {
+		log.Println("存储消息到 MongoDB 失败:", err)
 	}
 	appG.Response(http.StatusOK, e.SUCCESS, nil)
 }
--- backend/routers/router.go
+++ backend/routers/router.go
@@ -2,8 +2,10 @@ package routers
 
 import (
 	"discord-clone/middleware/jwt"
+	"discord-clone/pkg/upload"
 	"discord-clone/routers/api"
 	v1 "discord-clone/routers/api/v1"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -28,10 +30,11 @@ func InitRouter() *gin.Engine {
 	r.GET("/captcha", api.GetClickBasicCaptData)
 	r.POST("/verifyCaptcha", api.VerifyCaptcha)
 
+	r.StaticFS("/upload/images", http.Dir(upload.GetImageFullPath()))
+	r.GET("/ws", v1.WSHandler)
 	apiv1 := r.Group("/api/v1")
 	apiv1.Use(jwt.JWT())
 	{
-		apiv1.GET("/ws", v1.WSHandler)
 		// 获取所有频道
 		apiv1.GET("/channels", v1.GetChannels)
 		// 创建频道
@@ -49,9 +52,12 @@ func InitRouter() *gin.Engine {
 		// 获取特定频道的所有消息
 		apiv1.GET("/channels/:channelID/messages", v1.GetChannelMessageByID)
 
+		//传输图片
+		apiv1.POST("/channels/:channelID/image", v1.CreateChannelImage)
+
 	}
 	{
-		// apiv1.POST("/user/image", api.UploadImage) //上传图片
+		apiv1.POST("/user/image", v1.UploadUserImage) //上传用户头像
 	}
 	return r
 }
